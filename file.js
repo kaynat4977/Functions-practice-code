68b36c4d38061d7699c7a8018ea787f72520a328
Kaynat Shabbir <shabbirkainat75@gmail.com>
2024-11-27 17:28:32 +0500

    Add files via upload

---
+++ file.js
@@ -0,0 +1,31 @@
+function Hello() {
+    document.write("Hello Everyone This is Me ... Kainat shabbir Husaain");
+}
+
+function world(){
+document.write("physical properties of protien ... !!!");
+}
+Hello();
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+world();
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+Hello();
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+world();
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+Hello();
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+world();
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
